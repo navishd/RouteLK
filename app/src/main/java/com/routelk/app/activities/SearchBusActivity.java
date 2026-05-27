4b2117c1259949ed6965ba8adac0b48490e37db2
Navishmitha Dakshith <navishmithadakshith@Navishmithas-MacBook-Air.local>
2026-05-27 21:56:13 +0530

    Create search bus screen UI

---
+++ app/src/main/java/com/routelk/app/activities/SearchBusActivity.java
@@ -0,0 +1,14 @@
+package com.routelk.app.activities;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.routelk.app.R;
+
+public class SearchBusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_bus);
+    }
+}
